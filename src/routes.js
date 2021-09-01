20fb3a35782fd4c92d9a71349ca151df5c8213b4
massenandev <massenandev@gmail.com>
2021-09-01 16:27:59 -0300

    feat: add route GET /users

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const User = require('./models/User')
 
 const router = Router()
 
@@ -6,4 +7,7 @@ router.get('/', (request, response) => {
   response.send('REST Back-end Challenge 20201209 Running')
 })
 
+router.get('/users', (request, response) => {
+  User.find({ status: 'published' }).then((users) => response.json(users))
+})
 module.exports = router
