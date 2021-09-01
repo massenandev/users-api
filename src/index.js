c1b22f5a4faaaa5981eece07f9ab2e4e41675b78
massenandev <massenandev@gmail.com>
2021-08-31 22:04:28 -0300

    feat: add mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+const mongoose = require('mongoose')
 const express = require('express')
 
-const app = express()
 
-app.listen(3000, () => console.log('App is running'))
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/users');
+  const app = express()
+  app.listen(3000, () => console.log('App is running'))
+}
