7d01bd80b0ada33195eb7b414d519d8de6e0af51
massenandev <massenandev@gmail.com>
2021-09-03 15:16:56 -0300

    feat: add API documentation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger.json')
 const axios = require('axios')
 const User = require('./models/User')
 const routes = require('./routes')
 
 main().catch(err => console.log(err));
-
 
 
 async function main() {
@@ -17,13 +18,10 @@ async function main() {
     }
   });
 
-  // axios.get('https://randomuser.me/api/?results=10')
-  // .then((response) => {
-  //   User.insertMany(response.data.results)
-  // });
 
   const app = express()
   app.use(express.json())
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
   app.use(routes)
   app.listen(3000, () => console.log('App is running'))
 }
